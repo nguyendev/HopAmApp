a94df81eedd2954180bc0e89ebbc904332e83cbe
nguyendev <nguyen.nah76@gmail.com>
2017-12-29 22:59:11 +0700

    Create Empty Project

---
+++ Code/NguyenIT/App.js
@@ -0,0 +1,79 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput
+
+  // StatusBar
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <StatusBar hidden="true"/> */}
+        <TextInput
+ style={styles.searchBar}
+ value=""
+ onChange=""
+ placeholder="Tìm kiếm bài hát" />
+
+        <Text style={styles.welcome}>
+          Hello Nguyên IT
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  searchBar: {
+  paddingLeft: 30,
+  fontSize: 22,
+  height: 10,
+  flex: 1,
+  borderWidth: 9,
+  borderColor: '#E4E4E4',
+},
+
+
+});
+
+
